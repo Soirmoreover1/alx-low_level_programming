d404962be158768cd5ce70693a839f21e329b2ce
mohamed <lal939877@gmail.com>
2023-08-07 06:02:40 -0700

    a

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -3,6 +3,7 @@
 /**
  * create_file - Creates a file.
  * @filename: A pointer to the name of the file to create.
+ *
  * @text_content: A pointer to a string to write to the file.
  *
  * Return: If the function fails - -1.
@@ -17,6 +18,7 @@ int create_file(const char *filename, char *text_content)
 
 	if (text_content != NULL)
 	{
+		for (l = 0; text_content[l];)
 			l++;
 	}
 
